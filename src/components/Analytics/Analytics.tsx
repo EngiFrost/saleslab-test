4da8ea86a1f6d04afc7caf1b71301d9f7d54bf96
EngiFrost <engifrost@gmail.com>
2022-10-10 13:24:03 +0300

    added & attached svg icons

--- src/components/Analytics/Analytics.tsx
+++ src/components/Analytics/Analytics.tsx
@@ -6,24 +6,55 @@ import './styles.css';
 export const Analytics: FC = () => {
   const cards: TCard[] = [
     {
-      amount: 123,
+      amount: 1600,
       color: 'purple',
-      img: '',
-      title: 'Won',
-      money: 108000, // TODO: format money (add spaces)
+      img: `${process.env.PUBLIC_URL}/svg/snowflake.svg`,
+      title: 'Total Leads and Deals',
+      money: 132000999,
     },
     {
-      amount: 123,
+      amount: 45,
       color: 'green',
-      img: '',
+      img: `${process.env.PUBLIC_URL}/svg/ok_mark.svg`,
       title: 'Won',
-      money: 108000, // TODO: format money (add spaces)
+      money: 108000,
     },
     {
-      amount: 123,
+      amount: 15,
       color: 'red',
-      img: '',
-      title: 'Won',
+      img: `${process.env.PUBLIC_URL}/svg/cross.svg`,
+      title: 'Lost',
+      money: 24000,
+    },
+    {
+      amount: 115,
+      color: 'blue',
+      img: `${process.env.PUBLIC_URL}/svg/person.svg`,
+      title: 'New Leads',
+    },
+    {
+      amount: 269,
+      color: 'purple',
+      img: `${process.env.PUBLIC_URL}/svg/doc.svg`,
+      title: 'Total tasks',
+    },
+    {
+      amount: 115,
+      color: 'green',
+      img: `${process.env.PUBLIC_URL}/svg/doc_ok.svg`,
+      title: 'Completed tasks',
+    },
+    {
+      amount: 45,
+      color: 'red',
+      img: `${process.env.PUBLIC_URL}/svg/doc_cross.svg`,
+      title: 'Expired tasks',
+    },
+    {
+      amount: 10,
+      color: 'blue',
+      img: `${process.env.PUBLIC_URL}/svg/doc_empty.svg`,
+      title: 'No tasks',
     },
   ];
 
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import { Analytics } from '../Analytics/Analytics';
-import './styles.css';
 
 export const App: FC = () => {
-  return <div className="app">
-    <Analytics />
-  </div>;
+  return (
+    <>
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
